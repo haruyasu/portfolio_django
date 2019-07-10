5872c60859c29319e2a78d16df906df3fc1f121d
haruyasu <harukun2002@gmail.com>
2019-07-09 21:10:40 -0400

    nav list

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # path('', views.PostListView.as_view(), name='post_list'),
     path('', views.HomeView.as_view(), name='home'),
     path('about/', views.AboutView.as_view(), name='about'),
+    path('list/', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
