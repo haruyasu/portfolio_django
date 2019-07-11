2ea438b0660da8cde29946191d7d2cebe9c601d7
haruyasu <harukun2002@gmail.com>
2019-07-10 20:48:35 -0400

    fix post_list

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from portfolio import views
 
 urlpatterns = [
-    # path('', views.PostListView.as_view(), name='post_list'),
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
-    path('list/', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
